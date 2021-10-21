256971b3a51c38d89b2f647d99665e4195c9f28d
vladtymo <tymo.vlad@gmail.com>
2021-10-21 17:09:12 +0300

    Add delegate empty project example

---
+++ 09_delegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09_delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
